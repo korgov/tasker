9697e19c9f8612a8741176cc039bf4416cfef7e4
Kirill Korgov <kirill@korgov.ru>
2013-04-06 19:46:19 +0400

    init2

---
+++ test/ru/korgov/tasker/ApplicationTest.java
@@ -0,0 +1,35 @@
+package ru.korgov.tasker;
+
+import org.junit.Test;
+import play.mvc.Content;
+import views.html.index;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static play.test.Helpers.contentAsString;
+import static play.test.Helpers.contentType;
+
+
+/**
+*
+* Simple (JUnit) tests that can call all parts of a play app.
+* If you are interested in mocking a whole application, see the wiki for more details.
+*
+*/
+public class ApplicationTest {
+
+    @Test 
+    public void simpleCheck() {
+
+        final int a = 1 + 1;
+        assertThat(a).isEqualTo(2);
+    }
+    
+    @Test
+    public void renderTemplate() {
+        final Content html = index.render("Your new application is ready.");
+        assertThat(contentType(html)).isEqualTo("text/html");
+        assertThat(contentAsString(html)).contains("Your new application is ready.");
+    }
+  
+   
+}
